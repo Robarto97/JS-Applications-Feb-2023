d90da63c7fc4b8ea8fe3d68e15287d386fcee7c9
Robarto <robartpetrov@gmail.com>
2023-03-16 17:25:38 +0200

    100

--- Client Side Rendering/Fill-Dropdown/dropdown.js
+++ Client Side Rendering/Fill-Dropdown/dropdown.js
@@ -1,3 +1,55 @@
-function addItem() {
-    console.log('TODO:...');
+import { html, render } from "./node_modules/lit-html/lit-html.js";
+
+window.addEventListener("load", loadOptions);
+document
+  .querySelector('input[type="submit"]')
+  .addEventListener("click", addItem);
+const url = "http://localhost:3030/jsonstore/advanced/dropdown";
+
+async function loadOptions() {
+  try {
+    const res = await fetch(url);
+
+    if (!res.ok) {
+      throw new Error();
+    }
+    const data = await res.json();
+    renderOptionComponent(Object.values(data));
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+const optionTemplate = (options) => html`
+  ${options.map(
+    (option) => html`<option value="${option._id}">${option.text}</option>`
+  )}
+`;
+
+const renderOptionComponent = (options) => {
+  const menu = document.querySelector("#menu");
+  render(optionTemplate(options), menu);
+};
+
+async function addItem(e) {
+  e.preventDefault();
+  const itemText = document.querySelector("#itemText");
+  try {
+    if (!itemText.value) {
+      throw new Error(`It's empty!`);
+    }
+    const res = await fetch(url, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ text: itemText.value }),
+    });
+    if (!res.ok) {
+      throw new Error();
+    }
+    loadOptions()
+    itemText.value = ''
+
+  } catch (error) {
+    alert(error.message);
+  }
 }
--- Client Side Rendering/Table-Search/solution.js
+++ Client Side Rendering/Table-Search/solution.js
@@ -1,8 +1,52 @@
-function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+import { html, render } from "./node_modules/lit-html/lit-html.js";
 
-   function onClick() {
-      //   TODO:
+document.querySelector("#searchBtn").addEventListener("click", onClick);
+window.addEventListener("load", loadData);
 
-   }
+function onClick() {
+  const rows = Array.from(document.querySelectorAll("tbody tr"));
+  const searchInput = document.querySelector("#searchField");
+
+  if (!searchInput.value) {
+    alert("Write something");
+  } else {
+    rows.map((row) => {
+      row.classList.remove("select");
+      if (
+        row.innerHTML.toLowerCase().includes(searchInput.value.toLowerCase())
+      ) {
+        row.classList.add("select");
+      }
+    });
+    searchInput.value = "";
+  }
 }
+
+async function loadData() {
+  try {
+    const res = await fetch("http://localhost:3030/jsonstore/advanced/table");
+    if (!res.ok) {
+      throw new Error();
+    }
+    const data = await res.json();
+    renderRowTemplate(Object.values(data));
+  } catch (error) {
+    alert(error.message);
+  }
+}
+
+const renderRowTemplate = (rows) => {
+  const tbody = document.querySelector(".container tbody");
+  render(rowTemplate(rows), tbody);
+};
+
+const rowTemplate = (rows) => html`
+  ${rows.map(
+    (row) =>
+      html`<tr id=${row._id}>
+        <td>${row.firstName} ${row.lastName}</td>
+        <td>${row.email}</td>
+        <td>${row.course}</td>
+      </tr>`
+  )}
+`;
